610569adfb4e9f0733fa9ea67ba5f9e1d64d0fce
smallzee <tech4all583@gmail.com>
2024-05-11 09:22:54 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,15 +7,12 @@
             <div class="row">
                 <div class="col-xl-12 col-md-12">
                     <div class="banner-top__content">
-                        <div class="sale-top">
-                            <a href="#">75% SAVE</a>
-                            <p>For the Black Friday weekend</p>
-                        </div>
+
                         <h2 class="title">Powering Data for the new equity blockchain.</h2>
                         <p class="desc">The platform helps investors to make easy to purchase and sell their tokens</p>
 
                         <div class="button">
-                            <a class="btn-action" href="{{route('register')}}"><span>REGISTER & BUY TOKEN NOW</span><span class="icon-arrow"></span></a>
+                            <a class="btn-action" href="{{route('register')}}"><span>REGISTER & BUY PACKAGE</span></a>
                         </div>
                     </div>
                 </div>
@@ -69,4 +66,77 @@
         </div>
     </section>
     <!-- End Section Features -->
+
+    <!-- Section Choose -->
+    <section class="features" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block-text center">
+                        <h3 class="title">
+                            Why Choose Our Token?
+                        </h3>
+                        <p class="fs-17 mt-12">
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui official
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-md-6">
+                    <div class="choose__box" data-aos="fade-up" data-aos-duration="800">
+                        <div class="choose__box-icon center">
+                            <div class="icon">
+                                <span class="icon-mobile"></span>
+                            </div>
+                            <div class="content">
+                                <h5 class="h5">Mobie Payment</h5>
+                                <p class="mt-9">The process of taking into account the collective opinion of a group</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="choose__box" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="choose__box-icon center">
+                            <div class="icon">
+                                <span class="icon-startup"></span>
+                            </div>
+                            <div class="content">
+                                <h5 class="h5">Investment Projects</h5>
+                                <p class="mt-9">The platform helps investors to make easy to purchase and sell their tokens</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="choose__box" data-aos="fade-up" data-aos-duration="1200">
+                        <div class="choose__box-icon center">
+                            <div class="icon">
+                                <span class="icon-vuesax"></span>
+                            </div>
+                            <div class="content">
+                                <h5 class="h5">Protect the identity</h5>
+                                <p class="mt-9">The platform helps investors to make easy to purchase and sell their tokens</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="choose__box" data-aos="fade-up" data-aos-duration="1400">
+                        <div class="choose__box-icon center">
+                            <div class="icon">
+                                <span class="icon-mobile"></span>
+                            </div>
+                            <div class="content">
+                                <h5 class="h5">Security your money</h5>
+                                <p class="mt-9">The process of taking into account the collective opinion of a group</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Section Choose -->
 @endpush
