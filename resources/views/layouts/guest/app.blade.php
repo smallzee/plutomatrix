fa50630f1bc251f573d449b6a3f820d4e0a8a23b
smallzee <tech4all583@gmail.com>
2024-05-11 14:43:44 +0100

    update

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -14,6 +14,26 @@
 </head>
 <body>
 
+<!-- preloade -->
+<div class="preload preload-container">
+    <div class="preload-logo" style="background-image: url('images/logo/144.png')">
+        <div class="spinner"></div>
+    </div>
+</div>
+<!-- /preload -->
+
+<div class="header-style2 fixed-top bg-menuDark">
+    <div class="d-flex justify-content-between align-items-center gap-14">
+        <div class="box-account style-2">
+            <a href="{{route('dashboard.index')}}">
+                <img src="images/avt/avt2.jpg.png" alt="img" class="avt">
+            </a>
+        </div>
+
+    </div>
+</div>
+
+@include('layouts.guest.footer')
 
 <script type="text/javascript" src="{{asset('assets/guest/js/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('assets/guest/js/bootstrap.min.js')}}"></script>
--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -1,2 +1,28 @@
-<?php
-
+<div class="menubar-footer footer-fixed">
+    <ul class="inner-bar">
+        <li class="active">
+            <a href="home.html">
+                <i class="icon icon-home2"></i>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="exchange-market.html">
+                <i class="icon icon-exchange"></i>
+                Exchange
+            </a>
+        </li>
+        <li>
+            <a href="earn.html">
+                <i class="icon icon-earn"></i>
+                Earn
+            </a>
+        </li>
+        <li>
+            <a href="wallet.html">
+                <i class="icon icon-wallet"></i>
+                Wallet
+            </a>
+        </li>
+    </ul>
+</div>
