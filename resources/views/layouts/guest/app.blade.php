0819c9717d3284353f39201743964365db84e5b7
smallzee <tech4all583@gmail.com>
2024-05-11 13:42:31 +0100

    update

---
+++ resources/views/layouts/guest/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('assets/guest/fonts/fonts.css')}}">
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{asset('assets/guest/fonts/font-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/guest/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/guest/css/styles.css')}}"/>
+</head>
+<body>
+
+
+
+<script type="text/javascript" src="{{asset('assets/guest/js/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/guest/js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/guest/js/main.js')}}"></script>
+</body>
+</html>
