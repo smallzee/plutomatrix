184993d98170301b075fd134a4286a22ed7123bf
smallzee <tech4all583@gmail.com>
2024-05-13 14:07:52 +0100

    update

--- resources/views/emails/InvestmentTransaction.blade.php
+++ resources/views/emails/InvestmentTransaction.blade.php
@@ -1,11 +1,17 @@
 <x-mail::message>
-# Introduction
 
-The body of your message.
+<p>Dear, {{ $name }}</p>
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+<ol>
+    <li>Email Address : {{ $email }}</li>
+    <li>Amount : ${!! number_format($amount,2) !!}</li>
+    <li>Status : {{ $status }}</li>
+    <li>Reference : {{ $reference }}</li>
+    <li>Package : {{ $package }}</li>
+    <li>Method : {{ $method }}</li>
+    <li>Transaction Date : {{ date('Y-m-d h:i:s') }}</li>
+</ol>
+
 
 Thanks,<br>
 {{ config('app.name') }}
