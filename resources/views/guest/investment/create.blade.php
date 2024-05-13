8ada0d9970b33d6a9ad561c4395b98d862ac8e32
smallzee <tech4all583@gmail.com>
2024-05-13 11:09:28 +0100

    update

--- resources/views/guest/investment/create.blade.php
+++ resources/views/guest/investment/create.blade.php
@@ -4,20 +4,46 @@
 
     <div class="pt-20 pb-80">
         <div class="tf-container">
-
-            <div class="grid grid-cols-3">
+            <div class="row">
                 @foreach($packages as $value)
-                    <a href="">
+                    <div class="col-sm-6">
                         <div class="accent-box-v5 bg-menuDark mb-3 active">
                             <div class="mt-12">
-                                <h5 class="mb-3">{{ $value->name }}</h5>
-                                <ol>
-                                    <li>Minimum Deposit</li>
-                                    <li>${{ number_format($value->min_deposit,2) }}</li>
-                                </ol>
+                                <h4 class="mb-3 text-center">{{ ucwords($value->name) }}</h4>
+                                <table class="table table-bordered text-white">
+                                    <tr>
+                                        <td>Minimum Deposit : </td>
+                                        <td>${{ number_format($value->min_deposit,2) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Maximum Deposit : </td>
+                                        <td>${{ number_format($value->max_deposit,2) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Return : </td>
+                                        <td>{{ $value->interest }}%</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Duration : </td>
+                                        <td>{{ $value->duration }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Referral Percentage : </td>
+                                        <td>{{ $value->referral_percentage }}%</td>
+                                    </tr>
+                                </table>
+
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <a href="" class="tf-btn lg primary">Invest Now</a>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <a href="#" data-interest="{{ $value->interest }}" class="tf-btn lg primary">RIO Calculator</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 @endforeach
             </div>
 
